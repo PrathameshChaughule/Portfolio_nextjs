46662cca917026132dadd98ef56fc3b873d3876f
PrathameshChaughule <pvc14102002@gmail.com>
2026-04-28 19:23:02 +0530

    Responsive

--- app/page.js
+++ app/page.js
@@ -5,28 +5,33 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
-import Services from "./components/Services";
 import Work from "./components/Work";
 import Education from "./components/Education";
 
 export default function Home() {
+  const [dark, setDark] = useState(false);
 
-  const [dark, setDark] = useState(localStorage.getItem("theme") === "dark");
-  
-      useEffect(() => {
-          document.documentElement.setAttribute(
-              "data-theme",
-              dark ? "dark" : "light"
-          );
-          localStorage.setItem("theme", dark ? "dark" : "light");
-      }, [dark]);
+  // ✅ Run only on client
+  useEffect(() => {
+    const theme = localStorage.getItem("theme");
+    if (theme === "dark") {
+      setDark(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    document.documentElement.setAttribute(
+      "data-theme",
+      dark ? "dark" : "light"
+    );
+    localStorage.setItem("theme", dark ? "dark" : "light");
+  }, [dark]);
 
   return (
     <>
       <Navbar dark={dark} setDark={setDark}/>
       <Header dark={dark}/>
       <About dark={dark}/>
-      {/* <Services /> */}
       <Education />
       <Work dark={dark}/>
       <Contact />
